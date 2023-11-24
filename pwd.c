07731076bb86cef770e0f9543ef8a11c7b51cbe6
Zeineb <zeineb.ben-sassi@etu.u-paris.fr>
2023-11-24 11:00:28 +0100

    pwd function added

---
+++ pwd.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <linux/limits.h>
+
+#include "pwd.h"
+
+/*Prints current working directory*/
+int pwd(int nb_args){
+
+    if(nb_args!=1){
+        goto error ;
+    }
+
+    char pwd[PATH_MAX];
+    if (getcwd(pwd,PATH_MAX)==NULL){
+        perror("pwd : getcwd");
+        goto error; 
+    }
+    int nb_written= write(STDOUT_FILENO,pwd,strlen(pwd));
+    if (nb_written<strlen(pwd)) {
+        perror("pwd : write");
+        goto error; 
+    }
+    int line_break=write(STDOUT_FILENO,"\n",1);
+    if (line_break!=1){
+        perror("pwd : line break");
+        goto error; 
+    }
+    return 0;
+
+    error: 
+    return 1 ;
+}
+
+
+int main(int argc, char **argv) {
+if(argc!=1){
+    perror("pwd : argument number");
+    exit(1);
+}
+pwd(argc);
+
+  return 0;  
+}
---
+++ pwd.h
@@ -0,0 +1,6 @@
+#ifndef PWD_H
+#define PWD_H
+
+int pwd(int); 
+
+#endif
