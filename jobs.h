18a61e670aa75b5d0be2caf52d7cb02ba5426f41
Zeineb <zeineb.ben-sassi@etu.u-paris.fr>
2023-12-19 21:03:48 +0100

    jobs with argument

--- jobs.h
+++ jobs.h
@@ -3,9 +3,10 @@
 
 #include "processus.h" //???
 
-void printJobs(processus *);
-void printTableOfJobs(processus_table *);
-int jobs(bool argument,processus_table * processus_table);
+void print_table_of_certain_jobs(processus_table *, int);
+void print_jobs(processus *);
+void print_table_of_jobs(processus_table *);
+int jobs(bool, char *, processus_table *);
 void maj_main_print(processus_table* proc_table);
 
 #endif
